897aa8682d9f9714f4db24806cd504d812a81954
Anne Belka and Steve Burton <anne+steve@>
2016-12-01 10:45:24 -0800

    add user interface

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,6 +1,7 @@
 using Nancy;
 using System.Collections.Generic;
 using ScrabbleScore.Objects;
+using System;
 
 namespace ScrabbleScore
 {
@@ -10,7 +11,22 @@ namespace ScrabbleScore
 		{
 			Get["/"] = _ =>
 			{
-				return View["index.cshtml"];
+				Dictionary<string, int> objects = new Dictionary<string, int>();
+				objects["HasWord"] =0;
+				Console.WriteLine(objects["HasWord"]);
+				return View["index.cshtml", objects];
+			};
+
+			Post["index.cshtml"] = _ =>
+			{
+				Dictionary<string, int> objects = new Dictionary<string, int>();
+				objects["HasWord"] =1;
+				string userWord = Request.Form["word"];
+				Console.WriteLine(userWord);
+				Scrabble newScrabble = new Scrabble(userWord);
+				objects["ScrabbleScore"] = newScrabble.ScoreIs();
+				Console.WriteLine(objects["ScrabbleScore"]);
+				return View["index.cshtml", objects];
 			};
 		}
 	}
